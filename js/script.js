219a3c0e6d9b10e6f783517f8794565955555b0f
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-12 18:16:11 -0400

    sticky nav working correectly

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-  $(".latest-news").hide(); 
+  var $latestNews = $(".latest-news");
+  clearHero = $('.hero').outerHeight();
+  console.log($(window).scrollTop());
+
+  $latestNews.hide();
+
   setTimeout(function() {
     $(".headline").fadeOut(3500);
-    $(".latest-news").fadeIn(5000);
+    $latestNews.fadeIn(5000);
   }, 500);
+
+  $(window).scroll(function() {
+    console.log($(window).scrollTop());
+    if ( $(window).scrollTop() == 0) {
+      console.log('first')
+      $('nav').removeClass('fixed-top').next();
+      $('nav').addClass('fixed-bottom').next();
+    } else if ( ($(window).scrollTop() - clearHero) < 0 ) {
+      console.log("riding the hero");
+      $('nav').removeClass('fixed-bottom').next();
+      $('nav').removeClass('fixed-top').next();
+    } else {
+      console.log("fixed-top")
+      $('nav').addClass('fixed-top').next();
+    } 
+  });
+
 });
