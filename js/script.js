180467a01fc5e296c12674f0af05588d944bf1f2
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-18 15:39:05 -0400

    more updates to site

--- js/script.js
+++ js/script.js
@@ -126,4 +126,18 @@ $(document).ready(function() {
   });
 
   newPhotosCollection.renderCarousel();
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log(target)
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 20
+        }, "slow");
+        return false;
+      }
+    }
+  });
 });
