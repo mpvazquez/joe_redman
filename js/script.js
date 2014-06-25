fcdf69b3309596d2a4d4066b5c702c05cefe7434
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-25 16:09:43 -0400

    updated @media query for better rendering

--- js/script.js
+++ js/script.js
@@ -190,7 +190,7 @@ $(document).ready(function() {
     }
   })
 
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').on("click", function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -207,13 +207,11 @@ $(document).ready(function() {
     console.log('clicked');
   });
 
-  var backgroundWidth = $('.hero').css("background-size");
-
-  if($window.width() > parseInt(backgroundWidth)) {
-    $('.hero').css("background-size", $window.width() + "px auto");
+  if($window.width() > parseInt($('.hero').css("background-size") - 500)) {
+    // $('.hero').css("background-size", $window.width() + "px auto");
   }
-  if($window.height() > $('.hero').height()) {
-    $('.hero').css("height", $window.height())
+  if($window.height() > $('.hero').height() - $nav.height()) {
+    $('.hero').css("height", $window.height() + "px")
       .css("background-size", "cover");
   }
 });
