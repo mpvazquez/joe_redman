8f7a7086bf42642b1a0d907f04463e58d6793b16
Marco Vazquez <marcopvazquez@Marcos-MacBook-Pro.local>
2017-07-12 16:40:46 -0400

    Hide local variables from global scope

--- js/script.js
+++ js/script.js
@@ -1,263 +1,265 @@
-// photos view constructor function and prototype renders them
-var PhotosView = function(imageName, imageIndex){
-  this.imageName = imageName;
-  this.imageIndex = imageIndex;
-  this.el = $("<li>").addClass("back-position");
-}
-PhotosView.prototype.render = function(){
-  $("<img>").attr("src", "images/thumbnails/" + this.imageName)
-    .attr("id", imagesList['full-size-images'][this.imageIndex])
-    .addClass(imagesList['image-layouts'][this.imageIndex])
-    .appendTo(this.el);
-  this.el.append($("<p>").html(imagesList['image-descriptions'][this.imageIndex]));
-
-  $('#photo-carousel').append(this.el);
-  newPhotosCollection.models.push(this);
-  return this;
-}
-
-// collection of photos, image element models to render
-// as an image carousel that can expand/shrink
-var PhotosCollection = function(){
-  this.models = [];
-  this.startPosition = 1;
-}
-// renders photo collection carousel on page
-PhotosCollection.prototype.renderCarousel = function() {
-  for(var i = 0; i < this.photosCount(); i++) {
-    if(i === this.prevPhotoPosition()) {
-      $(this.models[i].el).removeClass()
-        .addClass("left-position");
-    } else if (i === this.startPosition) {
-      $(this.models[i].el).removeClass()
-        .addClass("center-position");
-    } else if (i === this.nextPhotoPosition()) {
-      $(this.models[i].el).removeClass()
-        .addClass("right-position");
-    }
-  }
-  this.addEventHandlers();
-  return this;
-}
-// returns total count of photo models in collection
-PhotosCollection.prototype.photosCount = function() {
-  return this.models.length;
-}
-// returns value of next photo in collection
-PhotosCollection.prototype.nextPhotoPosition = function() {
-  if(this.startPosition === this.photosCount() - 1) {
-    return 0;
-  } else {
-    return this.startPosition + 1;
-  }
-}
-// returns value of previous photo in collection
-PhotosCollection.prototype.prevPhotoPosition = function() {
-  if(this.startPosition === 0) {
-    return this.photosCount() - 1;
-  } else {
-    return this.startPosition - 1;
-  }
-}
-// lets user select next photo in collection 
-PhotosCollection.prototype.moveForward = function() {
-  //removes event handlers that were attached in current position
-  this.removeEventHandlers();
-
-  if(this.photosCount() - 1 === this.startPosition) {
-    $(this.models[this.photosCount() - 2].el).removeClass()
-      .addClass("back-position");
-    this.startPosition = 0;
-  } else {  
-    $(this.models[this.prevPhotoPosition()].el).removeClass()
-      .addClass("back-position");
-    this.startPosition++;
-  }
-  this.renderCarousel();
-  return this;
-}
-// lets user select previous photo oin collection
-PhotosCollection.prototype.moveBackward = function() {
-  this.removeEventHandlers();
-
-  if(this.startPosition === 0) {
-    $(this.models[this.nextPhotoPosition()].el).removeClass()
-      .addClass("back-position");
-    this.startPosition = this.photosCount() - 1;
-  } else {  
-    $(this.models[this.nextPhotoPosition()].el).removeClass()
-      .addClass("back-position");
-    this.startPosition--;
-  }
-  this.renderCarousel();
-  return this;
-}
-// adds event listeners so user can move carousel forward
-PhotosCollection.prototype.addEventHandlers = function() {
-  $(".center-position").on("click", function() {
-    if($(window).width() < 800) {
-      newPhotosCollection.moveForward();
-    } else {
-      window.open("images/" + $(this).find("img").attr("id"), "_blank");
-    }
-  });
-
-  if($(window).width() < 800) {
-    var description = $('#carousel .center-position p');
-    $(description).css("display", "none");
-    $("#carousel #carousel-item-description").empty()
-      .append($("<p>").append($(description).html()))
-      .append("<strong>Hi-Res Download</strong>")
-      .on("click", function() {
-        window.open("images/" + $('#carousel .center-position').find("img").attr("id"), "_blank");
-      });
-  }
-
-  $(".left-position").on("click", function() {
-    newPhotosCollection.moveBackward();
-  });
-  $(".right-position").on("click", function() {
-    newPhotosCollection.moveForward();
-  });
-}
-// removes event handlers -- called after an event is evoked
-PhotosCollection.prototype.removeEventHandlers = function() {
-  $("#carousel #carousel-item-description").off("click");
-  $(".center-position").off("click");
-  $(".right-position").off("click");
-  $(".left-position").off("click");
-}
-PhotosCollection.prototype.setPermanentHandlers = function() {
-  $("#arrow-left").on("click", function() {
-    newPhotosCollection.moveBackward();
-  });
-  $("#arrow-right").on("click", function() {
-    newPhotosCollection.moveForward();
-  });
-
-  $("body").on("keydown", function(event) {
-    if(event.keyCode === 39) {
-      newPhotosCollection.moveForward();
-    } else if (event.keyCode === 37) {
-      newPhotosCollection.moveBackward();
-    }
-  });
-}
-// new photo collection / start carousel
-var newPhotosCollection = new PhotosCollection();
-
-
-
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-// script below this function will run once the DOM loads
 $(document).ready(function() {
-  var $nav = $('nav');
-  var $window = $(window);
-  var $scrolled = $window.scrollTop();
-  var $latestNews = $('.latest-news');
-  var $clearViewport = $window.outerHeight();
+  // photos view constructor function and prototype renders them
+  var PhotosView = function(imageName, imageIndex){
+    this.imageName = imageName;
+    this.imageIndex = imageIndex;
+    this.el = $("<li>").addClass("back-position");
+  }
+  PhotosView.prototype.render = function(){
+    $("<img>").attr("src", "images/thumbnails/" + this.imageName)
+      .attr("id", imagesList['full-size-images'][this.imageIndex])
+      .addClass(imagesList['image-layouts'][this.imageIndex])
+      .appendTo(this.el);
+    this.el.append($("<p>").html(imagesList['image-descriptions'][this.imageIndex]));
 
-  $latestNews.hide();
-  $(".credits").hide();
-  $nav.addClass("fixed-bottom").next();
+    $('#photo-carousel').append(this.el);
+    newPhotosCollection.models.push(this);
+    return this;
+  }
 
-  setTimeout(function() {
-    $(".headline").fadeOut(3500, function() {
-      $(".headline").addClass("joe-redman")
-        .removeClass("headline");
-      if ($(window).width() > 800) {
-        $(".joe-redman").fadeIn(1000);
+  // collection of photos, image element models to render
+  // as an image carousel that can expand/shrink
+  var PhotosCollection = function(){
+    this.models = [];
+    this.startPosition = 1;
+  }
+  // renders photo collection carousel on page
+  PhotosCollection.prototype.renderCarousel = function() {
+    for(var i = 0; i < this.photosCount(); i++) {
+      if(i === this.prevPhotoPosition()) {
+        $(this.models[i].el).removeClass()
+          .addClass("left-position");
+      } else if (i === this.startPosition) {
+        $(this.models[i].el).removeClass()
+          .addClass("center-position");
+      } else if (i === this.nextPhotoPosition()) {
+        $(this.models[i].el).removeClass()
+          .addClass("right-position");
+      }
+    }
+    this.addEventHandlers();
+    return this;
+  }
+  // returns total count of photo models in collection
+  PhotosCollection.prototype.photosCount = function() {
+    return this.models.length;
+  }
+  // returns value of next photo in collection
+  PhotosCollection.prototype.nextPhotoPosition = function() {
+    if(this.startPosition === this.photosCount() - 1) {
+      return 0;
+    } else {
+      return this.startPosition + 1;
+    }
+  }
+  // returns value of previous photo in collection
+  PhotosCollection.prototype.prevPhotoPosition = function() {
+    if(this.startPosition === 0) {
+      return this.photosCount() - 1;
+    } else {
+      return this.startPosition - 1;
+    }
+  }
+  // lets user select next photo in collection 
+  PhotosCollection.prototype.moveForward = function() {
+    //removes event handlers that were attached in current position
+    this.removeEventHandlers();
+
+    if(this.photosCount() - 1 === this.startPosition) {
+      $(this.models[this.photosCount() - 2].el).removeClass()
+        .addClass("back-position");
+      this.startPosition = 0;
+    } else {  
+      $(this.models[this.prevPhotoPosition()].el).removeClass()
+        .addClass("back-position");
+      this.startPosition++;
+    }
+    this.renderCarousel();
+    return this;
+  }
+  // lets user select previous photo oin collection
+  PhotosCollection.prototype.moveBackward = function() {
+    this.removeEventHandlers();
+
+    if(this.startPosition === 0) {
+      $(this.models[this.nextPhotoPosition()].el).removeClass()
+        .addClass("back-position");
+      this.startPosition = this.photosCount() - 1;
+    } else {  
+      $(this.models[this.nextPhotoPosition()].el).removeClass()
+        .addClass("back-position");
+      this.startPosition--;
+    }
+    this.renderCarousel();
+    return this;
+  }
+  // adds event listeners so user can move carousel forward
+  PhotosCollection.prototype.addEventHandlers = function() {
+    $(".center-position").on("click", function() {
+      if($(window).width() < 800) {
+        newPhotosCollection.moveForward();
+      } else {
+        window.open("images/" + $(this).find("img").attr("id"), "_blank");
       }
     });
-    $latestNews.fadeIn(5000);
-  }, 500);
 
-  $window.scroll(function() {
-    if ( $window.scrollTop() <= 0) {
-      $nav.removeClass('fixed-top').next()
-        .css("padding-top", "0");
-      $nav.addClass('fixed-bottom').next();
-    } else if ($window.scrollTop() < $clearViewport) {
-      $nav.removeClass().next()
-        .css("padding-top", "0");;
-    } else {
-      $nav.addClass('fixed-top').next()
-        .css("padding-top", "40px");
+    if($(window).width() < 800) {
+      var description = $('#carousel .center-position p');
+      $(description).css("display", "none");
+      $("#carousel #carousel-item-description").empty()
+        .append($("<p>").append($(description).html()))
+        .append("<strong>Hi-Res Download</strong>")
+        .on("click", function() {
+          window.open("images/" + $('#carousel .center-position').find("img").attr("id"), "_blank");
+        });
     }
-    if(($nav.hasClass("fixed-bottom") && $('nav.fixed-bottom').css("width")) || ($nav.hasClass("fixed-top") && $('nav.fixed-top').css("width"))) {
-      $('nav.fixed-bottom, nav.fixed-top').css("width", "100%");
-    }
-  });
 
-  $window.resize(function() {
-    if($window.width() < 800) {
-      $(".joe-redman").css("display", "none");
-      return;
-    }
-    $(".joe-redman").css("display", "block");
-  });
+    $(".left-position").on("click", function() {
+      newPhotosCollection.moveBackward();
+    });
+    $(".right-position").on("click", function() {
+      newPhotosCollection.moveForward();
+    });
+  }
+  // removes event handlers -- called after an event is evoked
+  PhotosCollection.prototype.removeEventHandlers = function() {
+    $("#carousel #carousel-item-description").off("click");
+    $(".center-position").off("click");
+    $(".right-position").off("click");
+    $(".left-position").off("click");
+  }
+  PhotosCollection.prototype.setPermanentHandlers = function() {
+    $("#arrow-left").on("click", function() {
+      newPhotosCollection.moveBackward();
+    });
+    $("#arrow-right").on("click", function() {
+      newPhotosCollection.moveForward();
+    });
 
-  $.each(imagesList['image-thumbnails'], function(i, image) {
-    var imageIndex = i;
-    new PhotosView(image, imageIndex).render();
-  });
-
-  newPhotosCollection.renderCarousel().setPermanentHandlers();
-
-  //display credits section on web page
-  $("#credits a, .slider-menu a").on("click", function() {
-    if($(".credits").css("display") === "block") {
-      $(".credits").hide();
-    } else {
-      $(".credits").css("display", "block");
-    }
-  })
-
-  $('a[href*=#]:not([href=#])').on("click", function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, "slow");
-        return false;
+    $("body").on("keydown", function(event) {
+      if(event.keyCode === 39) {
+        newPhotosCollection.moveForward();
+      } else if (event.keyCode === 37) {
+        newPhotosCollection.moveBackward();
       }
-    }
-  });
+    });
+  }
+  // new photo collection / start carousel
+  var newPhotosCollection = new PhotosCollection();
 
-  $('#hamburger').on("click", function(e) {
-    e.stopPropagation();
-    if($('.page-wrapper').css("position") !== "absolute") {
-      $('.page-wrapper')
-        .css("width", $('.page-wrapper').width())
-        .css("position", "absolute");
 
-      $('section.content-layer').css("display", "block");
 
-      $('.page-wrapper')
-        .animate({"right": "30%"}, 
-        "slow", "swing");
-    }
-  });
+  /////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////
+  // script below this function will run once the DOM loads
+  $(document).ready(function() {
+    var $nav = $('nav');
+    var $window = $(window);
+    var $scrolled = $window.scrollTop();
+    var $latestNews = $('.latest-news');
+    var $clearViewport = $window.outerHeight();
 
-  $("body, .slider-menu a").on("click", function() {
-    if($('.page-wrapper').css("position") === "absolute") {
-      $('.page-wrapper').animate({"right": 0}, 
-        "slow", 'swing').promise().done(function() {
-        $(".page-wrapper")
-          .css("width", "auto")
-          .css("position", "inherit");
-        $("section.content-layer").css("display", "none");
+    $latestNews.hide();
+    $(".credits").hide();
+    $nav.addClass("fixed-bottom").next();
+
+    setTimeout(function() {
+      $(".headline").fadeOut(3500, function() {
+        $(".headline").addClass("joe-redman")
+          .removeClass("headline");
+        if ($(window).width() > 800) {
+          $(".joe-redman").fadeIn(1000);
+        }
       });
+      $latestNews.fadeIn(5000);
+    }, 500);
+
+    $window.scroll(function() {
+      if ( $window.scrollTop() <= 0) {
+        $nav.removeClass('fixed-top').next()
+          .css("padding-top", "0");
+        $nav.addClass('fixed-bottom').next();
+      } else if ($window.scrollTop() < $clearViewport) {
+        $nav.removeClass().next()
+          .css("padding-top", "0");;
+      } else {
+        $nav.addClass('fixed-top').next()
+          .css("padding-top", "40px");
+      }
+      if(($nav.hasClass("fixed-bottom") && $('nav.fixed-bottom').css("width")) || ($nav.hasClass("fixed-top") && $('nav.fixed-top').css("width"))) {
+        $('nav.fixed-bottom, nav.fixed-top').css("width", "100%");
+      }
+    });
+
+    $window.resize(function() {
+      if($window.width() < 800) {
+        $(".joe-redman").css("display", "none");
+        return;
+      }
+      $(".joe-redman").css("display", "block");
+    });
+
+    $.each(imagesList['image-thumbnails'], function(i, image) {
+      var imageIndex = i;
+      new PhotosView(image, imageIndex).render();
+    });
+
+    newPhotosCollection.renderCarousel().setPermanentHandlers();
+
+    //display credits section on web page
+    $("#credits a, .slider-menu a").on("click", function() {
+      if($(".credits").css("display") === "block") {
+        $(".credits").hide();
+      } else {
+        $(".credits").css("display", "block");
+      }
+    })
+
+    $('a[href*=#]:not([href=#])').on("click", function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 50
+          }, "slow");
+          return false;
+        }
+      }
+    });
+
+    $('#hamburger').on("click", function(e) {
+      e.stopPropagation();
+      if($('.page-wrapper').css("position") !== "absolute") {
+        $('.page-wrapper')
+          .css("width", $('.page-wrapper').width())
+          .css("position", "absolute");
+
+        $('section.content-layer').css("display", "block");
+
+        $('.page-wrapper')
+          .animate({"right": "30%"}, 
+          "slow", "swing");
+      }
+    });
+
+    $("body, .slider-menu a").on("click", function() {
+      if($('.page-wrapper').css("position") === "absolute") {
+        $('.page-wrapper').animate({"right": 0}, 
+          "slow", 'swing').promise().done(function() {
+          $(".page-wrapper")
+            .css("width", "auto")
+            .css("position", "inherit");
+          $("section.content-layer").css("display", "none");
+        });
+      }
+    });
+
+    if($window.width() > parseInt($('.hero').css("background-size"))) {
+      $('.hero, .contact-hero').css("background-size", $window.width() + "px auto");
+    }
+    if($window.height() > $('.hero').height()) {
+      $('.hero, .contact-hero').css("height", $window.height() + "px");
     }
   });
-
-  if($window.width() > parseInt($('.hero').css("background-size"))) {
-    $('.hero, .contact-hero').css("background-size", $window.width() + "px auto");
-  }
-  if($window.height() > $('.hero').height()) {
-    $('.hero, .contact-hero').css("height", $window.height() + "px");
-  }
 });
